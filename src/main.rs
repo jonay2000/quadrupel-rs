0332edaf06640ce7dd9d7b4b93a28d9fb2b0e3cf
jonathan <jonathantbrouwer@gmail.com>
2022-04-21 10:12:01 +0200

    Add alloc

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,12 @@
 #![no_std]
 #![no_main]
+#![feature(alloc_error_handler)]
 
+extern crate alloc;
+
+use alloc::vec;
+use core::alloc::Layout;
+use alloc_cortex_m::CortexMHeap;
 // pick a panicking behavior
 use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
 // use panic_abort as _; // requires nightly
@@ -11,12 +17,26 @@ use cortex_m::asm;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
 
+#[global_allocator]
+static ALLOCATOR: CortexMHeap = CortexMHeap::empty();
+const HEAP_SIZE: usize = 1024; // in bytes
+
 #[entry]
 fn main() -> ! {
+    unsafe { ALLOCATOR.init(cortex_m_rt::heap_start() as usize, HEAP_SIZE) }
+
     hprintln!("Hello, world!");
     asm::nop(); // To not have main optimize to abort in release mode, remove when you add code
+
+    let vec = vec![1,2,3,4];
+    hprintln!("Hey {:?}", vec);
 
     loop {
         // your code goes here
     }
+}
+
+#[alloc_error_handler]
+fn alloc_error(_layout: Layout) -> ! {
+    panic!("Alloc error!");
 }
