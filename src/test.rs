e15c0910123a2fd250ff9ccb4e422f9b94c913b7
jonay2000 <jonabent@gmail.com>
2022-04-21 17:28:28 +0200

    test testing

--- src/test.rs
+++ src/test.rs
@@ -1,9 +1,17 @@
 use cortex_m_semihosting::debug::{exit, EXIT_SUCCESS};
+use cortex_m_semihosting::hprintln;
 
 pub fn test_runner(tests: &[&dyn Fn()]) {
+    hprintln!("--- RUNNING {} TESTS ---", tests.len());
     for test in tests {
         test();
     }
 
+    hprintln!("--- ALL TESTS SUCCESSFUL ---");
     exit(EXIT_SUCCESS);
+}
+
+#[test_case]
+fn test() {
+    assert_eq!(3 + 3, 6);
 }
