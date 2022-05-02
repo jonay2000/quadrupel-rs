6ca8513f26eb1b553bed2046b27cfc621b986471
noahiscool13 <noah@gamestoplay.nl>
2022-05-02 13:08:46 +0200

    Add PID

---
+++ quadrupel-dronecode/src/library/pid.rs
@@ -0,0 +1,31 @@
+use crate::library::yaw_pitch_roll::FI32;
+
+pub struct PID {
+    kp : FI32,
+    ki : FI32,
+    kd : FI32,
+    last_state: FI32,
+    buildup: FI32
+}
+
+impl PID {
+    pub fn new(kp : FI32, ki : FI32, kd : FI32) -> Self {
+        PID {
+            kp,
+            ki,
+            kd,
+            last_state: FI32::from_num(0),
+            buildup: FI32::from_num(0),
+        }
+    }
+
+    pub fn step(&mut self, dt : FI32, state: FI32, goal:FI32) -> FI32 {
+        let p_term = (goal-state)*self.kp;
+        let d_term = (self.last_pos - state) * self.kd /dt;
+        self.last_pos = state;
+        self.buildup += (goal-state)*dt;
+        self.buildup = self.buildup.clamp(FI32::from_num(-10), FI32::from_num(10));
+        i_term = self.buildup*self.ki;
+        p_term+d_term+i_term
+    }
+}
