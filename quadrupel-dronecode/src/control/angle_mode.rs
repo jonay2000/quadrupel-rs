3b0ce1e0b4ea313a2f44e70acf1a62730f3bdcaa
noahiscool13 <noah@gamestoplay.nl>
2022-05-04 11:45:09 +0200

    Add minimal motor speeds

--- quadrupel-dronecode/src/control/angle_mode.rs
+++ quadrupel-dronecode/src/control/angle_mode.rs
@@ -1,3 +1,4 @@
+use core::cmp::max;
 use crate::library::pid::PID;
 use crate::library::yaw_pitch_roll::FI32;
 
@@ -13,33 +14,31 @@ pub struct AngleMode {
 }
 
 impl AngleMode {
-    pub fn step(&mut self,dt:FI32, thrust: FI32, yaw_state: FI32, pitch_state:FI32,roll_state:FI32, yaw_goal: FI32,pitch_goal:FI32,roll_goal:FI32) -> [FI32; 4] {
-
-
-        let yaw_off = if ENABLE_YAW {
-            self.yaw_pid.step(dt,yaw_state,yaw_goal)
+    pub fn step(&mut self, dt: FI32, thrust: FI32, yaw_state: FI32, pitch_state: FI32, roll_state: FI32, yaw_goal: FI32, pitch_goal: FI32, roll_goal: FI32) -> [FI32; 4] {
+        let yaw_off = if ENABLE_YAW && thrust > 0 {
+            self.yaw_pid.step(dt, yaw_state, yaw_goal)
         } else {
             FI32::from_num(0)
         };
 
-        let pitch_off =  if ENABLE_PITCH {
-            self.pitch_pid.step(dt,pitch_state,pitch_goal)
+        let pitch_off = if ENABLE_PITCH && thrust > 0 {
+            self.pitch_pid.step(dt, pitch_state, pitch_goal)
         } else {
             FI32::from_num(0)
         };
 
-        let roll_off =  if ENABLE_ROLL {
-            self.roll_pid.step(dt,roll_state,roll_goal)
+        let roll_off = if ENABLE_ROLL && thrust > 0 {
+            self.roll_pid.step(dt, roll_state, roll_goal)
         } else {
             FI32::from_num(0)
         };
 
 
         return [
-            thrust + yaw_off + pitch_off,
-            thrust - yaw_off + roll_off,
-            thrust + yaw_off - pitch_off,
-            thrust - yaw_off - roll_off
-        ]
+            (thrust + yaw_off + pitch_off).max(FI32::from_num(0)),
+            (thrust - yaw_off + roll_off).max(FI32::from_num(0)),
+            (thrust + yaw_off - pitch_off).max(FI32::from_num(0)),
+            (thrust - yaw_off - roll_off).max(FI32::from_num(0))
+        ];
     }
 }
