a4bc78d4ad2ba5e24024ec9f6cb23eab92d79449
Jonathan Brouwer <jonathantbrouwer@gmail.com>
2022-06-03 15:29:55 +0200

    Kalman

--- quadrupel-dronecode/src/control/raw_mode.rs
+++ quadrupel-dronecode/src/control/raw_mode.rs
@@ -28,9 +28,10 @@ impl RawMode {
         let a_xi_1 = FI32::from_num(2)/a_yi;
         let a_xi_2 = FI32::from_num(1)/a_yi;
 
-        let kal_q_angle = FI64::from_num(0.00321373);
-        let kal_q_bias = FI64::from_num(-0.000167);
-        let kal_r_measure = FI64::from_num( 0.00483083);
+        //3.32858877e-05 5.51620221e-03 6.48176954e-05
+        let kal_q_angle = FI64::from_num(0.0000332858877);
+        let kal_q_bias = FI64::from_num(0.00551620221);
+        let kal_r_measure = FI64::from_num( 0.0000648176954);
 
         RawMode {
             yaw: FI64::from_num(0),
